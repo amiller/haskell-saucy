46b2c7491aced93ba9410495503133eaae522b66
Andrew Miller <socrates1024@gmail.com>
2017-11-23 15:31:44 -0600

    Switched to implicit params instead of monad stacks

--- ProcessIO.hs
+++ ProcessIO.hs
@@ -1,20 +1,15 @@
-{-# LANGUAGE FlexibleInstances, FlexibleContexts, UndecidableInstances,
-  RankNTypes, OverlappingInstances
+{-# LANGUAGE Rank2Types, ImplicitParams
   #-} 
 
-{- It's possible to use Haskell's ugly IO channels as a way of implementing message passing. 
+{- It's possible to use Haskell's channels as a way of implementing message passing. 
 
-   Typing is very lose.
-   It does not guarantee that:
+   However, the type restrictions here are looser than in ILC.
+   Control.Concurrent does not guarantee that:
    - there is only one active reader for each channel
    - a read is followed by at most one write
    - channels are not passed over channels
-   - closures are not passed over channels
 
    But we can still use this technique as a way of implementing these.
-
-   Finally-Tagless-Pi is a way of implementing a Pi-like language using this technique.
-   https://mail.haskell.org/pipermail/haskell-cafe/2010-June/078780.html
 
  -}
 
@@ -25,7 +20,6 @@ module ProcessIO where
 
 import Control.Concurrent.MonadIO
 import Control.Monad (forever, forM_, replicateM_)
-import Control.Monad.Reader
 
 import System.Random
 
@@ -33,7 +27,7 @@ data Void
 instance Show Void where
     show = undefined
 
-{-- Simple utilities channels --}
+{-- Simple utilities for working with channels --}
 wrapWrite f c = do
   d <- newChan 
   fork $ forever $ readChan d >>= writeChan c . f 
@@ -46,10 +40,10 @@ wrapRead f c = do
 
 assert cond msg = if not cond then fail msg else return ()
 
-{- Syntax sugar for parallel composition -}
-(|||) :: HasFork m => m () -> m () -> m ()
-p ||| q = fork p >> q
-infixl 1 |||
+{- Syntax sugar for parallel composition, returning the right value -}
+(|.) :: HasFork m => m () -> m () -> m ()
+p |. q = fork p >> q
+infixl 1 |.
 
 multiplex :: HasFork m => Chan a -> Chan b -> m (Chan (Either a b))
 multiplex a b = do
@@ -65,79 +59,36 @@ demultiplex ab a b = forever $ do
                          Left  y -> writeChan a y
                          Right y -> writeChan b y
 
-                                                    
-{- Several monad typeclasses are useful for composing processes.
-   These classes effectively just involve keeping track of special channels.
-   They are implemented using the ReaderT monad transformer.  -}
-
--- We need the following instance showing that fork works with ReaderT
-instance HasFork m => HasFork (ReaderT s m) where
-    fork p = do
-      s <- ask
-      lift $ fork $ runReaderT p s
-
 
 {- Randomized Algorithms -}
 {- Processes can request random bits. Under the hood, these are implemented as ordinary channels -}
-class HasFork m => MonadRand m where
-    getBit :: m Bool
 
-type RandomMonadT m = ReaderT (Chan (), Chan Bool) m
-    
-instance HasFork m => MonadRand (RandomMonadT m) where
-    getBit = do 
-      (ri,ro) <- ask
-      writeChan ri ()
-      readChan ro
+runRand :: HasFork m => ((?getBit :: m Bool) => m a) -> m a
+runRand p = let ?getBit = (liftIO $ randomRIO (False,True)) in p
 
-instance MonadRand m => MonadRand (ReaderT s m) where
-    getBit = lift getBit
-
+--getNbits :: (Num a, Monad m, ?getBit :: m Bool) => Int -> m a
 getNbits 0 = return 0
 getNbits n | n < 0 = fail "negative bits?"
 getNbits n = do
-  b <- getBit
+  b <- ?getBit
   rest <- getNbits (n-1)
   return $ 2*rest + if b then 0 else 1
 
-get32bytes :: (Num a, MonadRand m) => m a
+get32bytes :: (Num a, Monad m, ?getBit :: m Bool) => m a
 get32bytes = getNbits (32*8)
 
-runRand :: HasFork m => RandomMonadT m a -> m a
-runRand p = do
-  ri <- newChan
-  ro <- newChan
-  fork $ forever $ readChan ro >>= (const $ liftIO $ randomRIO (False,True)) >>= writeChan ri
-  runReaderT p (ro, ri)
+--flippedRand :: (HasFork m, ?getBit :: m Bool) => ((?getBit :: m Bool) => m a) -> m a
+flippedRand f = let ?getBit = ?getBit >>= (return . not) in f
 
-_flippedRand :: MonadRand m => (forall n. MonadRand n => n a) -> m a
-_flippedRand f = do
-  ri <- newChan
-  ro <- newChan
-  fork $ forever $ do
-                   () <- readChan ri
-                   b <- getBit
-                   liftIO $ putStrLn $ "inner flip: " ++ show b
-                   writeChan ro (not b)
-  runReaderT f (ri, ro)
-
-test2 :: MonadRand m => m Bool
---test2 = runRand getBit
-test2 = runRand $ _flippedRand getBit
+test2 :: IO Bool
+--test2 = runRand ?getBit
+test2 = runRand $ flippedRand ?getBit
 
 
 {- Provides a default channel to send on, when no message is intended -}
-class HasFork m => MonadDefault m where
-    pass :: m ()
 
-instance (HasFork m) => MonadDefault (ReaderT (Chan ()) m) where
-    pass = ask >>= flip writeChan ()
-
-instance MonadDefault m => MonadDefault (ReaderT s m) where
-    pass = lift pass
-      
-runDefault :: HasFork m => Chan () -> ReaderT (Chan ()) m a -> m a
-runDefault = flip runReaderT
+runDefault :: HasFork m => Chan () -> ((?pass :: m ()) => m a) -> m a
+runDefault c f = let ?pass = writeChan c () in f
 
 
 {- Simple examples -}
@@ -147,7 +98,7 @@ doubler i o = do
   writeChan o (x*2)
 
 flipWrite a b = do
-  x <- getBit
+  x <- ?getBit
   if x then writeChan a ()
   else      writeChan b ()
 
@@ -162,14 +113,12 @@ counter a b = do
   counter' 0
 
 -- Ask for a random bit, and print a message according to which one
-test1 :: MonadRand m => m ()
+test1 :: (HasFork m, ?getBit :: m Bool) => m ()
 test1 = do
   a <- newChan
   b <- newChan
   fork $ counter a b
   replicateM_ 10 $ flipWrite a b
 
---test1run :: IO ()
---test1run = do { runRand $ test1; threadDelay 1000}
-
-
+test1run :: IO ()
+test1run = do { runRand $ test1; threadDelay 1000}
